d39a78ea554568911c2ff5ebd4ef35da72a4b6d6
0xNerv <pepitomusicuwu@gmail.com>
2024-10-07 00:14:12 -0300

    Update ranger.py

--- ranger.py
+++ ranger.py
@@ -11,8 +11,9 @@ banner = '''
   / ___/ __ `/ __ \\/ __ `/ _ \\/ ___/
  / /  / /_/ / / / / /_/ /  __/ /    
 /_/   \\__,_/_/ /_/\\__, /\\___/_/     
-                 /____/         \033[4mv1.0\033[0m
-'''
+                 /____/           \033[4mv1.0\033[0m
+                           '''
+
 print(banner)
 
 def main(start, end, target, port, version):
@@ -24,21 +25,29 @@ def main(start, end, target, port, version):
         try:
             s.connect((url, int(port)))
             if version:
-               msg = s.recv(343).decode()
-               if not msg:
-                  msg = 'No encontrada'
+                msg = s.recv(343).decode()
+                if not msg:
+                    msg = 'No encontrada'
             
             try:
                 subdominio = socket.gethostbyaddr(url)
             except:
                 subdominio = ['No encontrado']
-            
+
+               
+            if output:
+               out = open(output,'a')
+               if version:
+                  out.write(f'IP: {url} Dominio: {subdominio[0]} Version: {msg}\n')
+               else:
+                  out.write(f'IP: {url} Dominio: {subdominio[0]}\n')
+
             if version:
-               print(f"[{Fore.GREEN}NEW\033[0m{Fore.RESET}] {Fore.RED}IP\033[0m{Fore.RESET}: {url}{Fore.BLUE}   {Fore.BLUE}Dominio\033[0m{Fore.RESET}: {subdominio[0]}   {Fore.RED}Version\033[0m{Fore.RESET}: {msg.replace('\n','')}".replace('\n',''))
+                print(f"[{Fore.GREEN}NEW\033[0m{Fore.RESET}] {Fore.RED}IP\033[0m{Fore.RESET}: {url}{Fore.BLUE}   {Fore.BLUE}Dominio\033[0m{Fore.RESET}: {subdominio[0]}   {Fore.RED}Version\033[0m{Fore.RESET}: {msg.replace('\n','')}".replace('\n',''))
             else:
-               sys.stdout.write(f'                           {Fore.BLUE}Dominio\033[0m{Fore.RESET}: {subdominio[0]}')
-               sys.stdout.write(f'\r[{Fore.GREEN}NEW{Fore.RESET}\033[0m] {Fore.RED}IP{Fore.RESET}\033[0m: {url}\n')
-               s.close()
+                sys.stdout.write(f'                           {Fore.BLUE}Dominio\033[0m{Fore.RESET}: {subdominio[0]}')
+                sys.stdout.write(f'\r[{Fore.GREEN}NEW{Fore.RESET}\033[0m] {Fore.RED}IP{Fore.RESET}\033[0m: {url}\n')
+                s.close()
         except KeyboardInterrupt:
             exit()
         except socket.timeout:
@@ -56,8 +65,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Escaneo de rango de subredes y puertos.')
     parser.add_argument('-t', '--target', required=True, help='Dirección IP con fuzzing, ejemplo: 192.168.0.fuzz')
     parser.add_argument('-p', '--port', required=True, help='Puerto a escanear')
-    parser.add_argument('-v','--version', action='store_true', help='Obtener la versión del servicio')
-    
+    parser.add_argument('-v', '--version', action='store_true', help='Obtener la versión del servicio')
+    parser.add_argument('-th', '--threads', required=True, type=int, help='Número de hilos a usar')
+    parser.add_argument('-o','--output', required=False,type=str,help='Output del escaneo')
+
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -66,12 +77,19 @@ if __name__ == "__main__":
     version = args.version
     target = args.target
     port = args.port
+    num_threads = args.threads
+    output = args.output
 
-    print(f'[{Fore.GREEN}NEW\033[0m{Fore.RESET}] {Fore.RESET}Creado por \033[4mBreach\033[0m {Fore.RESET}')
-    print(f'[{Fore.BLUE}INF\033[0m{Fore.RESET}] Principal{Fore.RESET}: {target}{Fore.RESET}\033[0m \n[{Fore.BLUE}INF{Fore.RESET}\033[0m] Puerto{Fore.RESET}: {Fore.GREEN}{port}\033[0m{Fore.RESET}')
+    print(f'[{Fore.GREEN}NEW\033[0m{Fore.RESET}] {Fore.RESET}Creado por \033[0mhttps://github.com/0xNerv\033[0m {Fore.RESET}')
+    print(f'[{Fore.BLUE}INF\033[0m{Fore.RESET}] Principal{Fore.RESET}: {target}{Fore.RESET}\033[0m \n[{Fore.BLUE}INF{Fore.RESET}\033[0m] Puerto{Fore.RESET}: {port}\033[0m{Fore.RESET}')
+    print(f'[{Fore.BLUE}INF{Fore.RESET}] Hilos: {num_threads}')
+    if version:
+       print(f'[{Fore.BLUE}INF{Fore.RESET}] Version: ON')
     print(f'[{Fore.GREEN}NEW{Fore.RESET}\033[0m] Escaneando subred ...')
-    
-    num_threads = 80
+    if output:
+       print (f'[{Fore.BLUE}INF{Fore.RESET}] Guardando output en: {output}')
+       out = open(output,'w')
+
     max_range = 255
     step = max_range // num_threads
 
@@ -85,4 +103,4 @@ if __name__ == "__main__":
         for future in futures:
             future.result()
 
-    print(f'[{Fore.BLUE}INF\033[0m{Fore.RESET}] Escaneo terminado.                \n')
+    print(f'[{Fore.BLUE}INF\033[0m{Fore.RESET}] Escaneo terminado.      ')
